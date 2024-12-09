8b2eba481de714ebfdc119ccfc463468071d528d
Marycielo29 <marycielomb29@gmail.com>
2024-12-09 17:24:00 -0500

    .

--- views/secciones/view_secciones.php
+++ views/secciones/view_secciones.php
@@ -56,7 +56,7 @@
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">REGISTRO DE SECCIONES</h5>
+        <h5 class="modal-title" id="exampleModalLabel">REGISTRO DE ALUMNOS</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
